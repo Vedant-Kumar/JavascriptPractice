4e3529a8a4589f7f03c2906d06759be38811bc13
Vedant-Kumar <42078597+Vedant-Kumar@users.noreply.github.com>
2026-07-25 05:50:34 +0000

    Comparison of Datatypes

--- Basics/basicprac.js
+++ Basics/basicprac.js
@@ -75,6 +75,32 @@
 // console.log(gameCounter);
 // console.log(gameCounter);
 
+// Comparison of Datatypes :
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log("2" == 2);
+// console.log("2" === 2);
+
+
+
+
+
+
+
+
+
 
 
 
