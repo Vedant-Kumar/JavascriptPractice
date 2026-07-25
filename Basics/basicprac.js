a5ee2c9b88ed99d7afa136c237cf8e0ea46a622b
Vedant-Kumar <42078597+Vedant-Kumar@users.noreply.github.com>
2026-07-25 03:42:49 +0000

    Datatypes

---
+++ Basics/basicprac.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// First Code:
+// console.log("Hello World, I'm here to conquer!!");
+
+
+// Variables :
+// let accountId = 1232323;
+// let accountEmail = "vedant8999@gmail.com";
+// let accountState;
+// var accountPassword = "231231 ";
+// accountCity = "Mumbai"
+// console.log(accountId);
+// accountEmail = "hello@google.com";
+// accountPassword = "1212213";
+// accountCity = "Pondicherry";
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+//Datatypes and ECMA Types :
+// let myName = "Vedant Kumar";
+// let age = 27;
+// let isLoggedin = true;
+// null 
+// undefined
+// symbol => unique identifier
+// object
+// console.log(typeof age); 
+
+
+
+
