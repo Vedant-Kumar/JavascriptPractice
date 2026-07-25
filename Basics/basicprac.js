af56f89e77a1601751cb91e0b58157ca16fde86f
Vedant-Kumar <42078597+Vedant-Kumar@users.noreply.github.com>
2026-07-25 04:41:40 +0000

    Conversions again

--- Basics/basicprac.js
+++ Basics/basicprac.js
@@ -39,6 +39,48 @@
 // "33abc" => NaN
 // true => 1
 
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// Operations :
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let str1 = "Hello";
+// let str2 = " Vedant";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(typeof(1 + "2"));
+// console.log(typeof("1" + 2));
+// console.log(typeof("1" + "2"));
+// console.log(typeof(1 + 2));
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// let gameCounter = 100;
+// console.log(gameCounter);
+// console.log(gameCounter);
+// ++gameCounter; 
+// ++gameCounter;
+// console.log(gameCounter);
+// console.log(gameCounter);
+// gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter);
+// console.log(gameCounter);
+
+
+
+
+
+
+
 
 
 
